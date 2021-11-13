3e1e73c07f104079b201e29570111b6373abf699
Rafael Steiner <steinraf@student.ethz.ch>
2021-11-13 19:28:49 +0100

    Added simple NN as basic_nn demo

--- basic_nn.py
+++ basic_nn.py
@@ -4,43 +4,67 @@ import random
 
 ti.init(arch=ti.gpu)
 
-x = ti.field(dtype=ti.f32, shape=(), needs_grad=True)
+input_size = 4
+
+x = ti.field(dtype=ti.f32, shape=4, needs_grad=True)
 weight = ti.field(dtype=ti.f32, shape=(), needs_grad=True)
 bias = ti.field(dtype=ti.f32, shape=(), needs_grad=True)
+output = ti.field(dtype=ti.f32, shape=(), needs_grad=True) #Somehow also needs grad even thought its is never accessed
 
 loss = ti.field(dtype=ti.f32, shape=(), needs_grad=True)
 
-learning_rate = 1e-3
+
+learning_rate = 1e-2
 
 
 def init():
-    x[None] = random.random() * 100 - 50
+    for i in range(input_size):
+        x[i] = random.random() * 100 - 50
     weight[None] = random.random() * 100 - 50
     bias[None] = random.random() * 100 - 50
-    loss[None] = 1
+    loss[None] = 0.0
 
-@ti.func
-def calculate_loss(value):
-    loss[None] = abs(value - 5)
 
 @ti.kernel
+def calculate_loss():
+    loss[None] = abs(output[None] - 5)
+
+
+def clear_output():
+    output[None] = 0
+
+
+@ti.kernel
+def get_output():
+    for i in range(input_size):
+        output[None] += x[i]*weight[None] + bias[None]
+
+
 def do_iter():
-    output = x[None]*weight[None] + bias[None]
-    calculate_loss(output)
+    get_output()
+    #print("Output is ", output[None])
+    calculate_loss()
+    #print("Loss is", loss[None])
 
 
 def update_variables():
-    x[None] -= learning_rate * x.grad[None]
+    for i in range(input_size):
+        x[i] -= learning_rate * x.grad[i]
+        #print("X_grad is ", x.grad[i])
     weight[None] -= learning_rate * weight.grad[None]
+    #print("weight_grad is ", weight.grad[None])
     bias[None] -= learning_rate * bias.grad[None]
+    #print("bias_grad is ", bias.grad[None])
 
 init()
 
-print("Initial x is: ", x[None], "Initial weight is: ", weight[None], "Initial bias is: ", bias[None], "Initial result is", x[None]*weight[None] + bias[None])
-
+#print("Initial x is: ", x[None], "Initial weight is: ", weight[None], "Initial bias is: ", bias[None], "Initial result is", x[None]*weight[None] + bias[None])
+print("Starting NN...")
 num_iter = 0
 while True:
     num_iter += 1
+    if num_iter % 100 == 0: print("Loss at iteration ", num_iter, " is ", loss[None])
+    clear_output()
     with ti.Tape(loss):
         do_iter()
     if loss[None] > 0.1: update_variables()
@@ -50,5 +74,4 @@ while True:
 print("Took ", num_iter, "iterations and ended with loss of ", loss[None])
 
 
-print("New x is: ", x[None], "New weight is: ", weight[None], "New bias is: ", bias[None], "New result is ", x[None]*weight[None] + bias[None])
-
+#print("New x is: ", x[None], "New weight is: ", weight[None], "New bias is: ", bias[None], "New result is ", x[None]*weight[None] + bias[None])
