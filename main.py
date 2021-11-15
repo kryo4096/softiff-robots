20608e97c86af5a3df404d55acdbe2682a25e63b
Jonas Luther <kryo4096@gmail.com>
2021-11-15 15:42:35 +0100

    working softbody sim

--- main.py
+++ main.py
@@ -7,9 +7,13 @@ class Mesh:
     def __init__(self):
         self.vertices = []
         self.indices = []
+        self.velocities = []
+        self.colors = []
 
-    def add_vertex(self,v):
+    def add_vertex(self,v,p=(0,0), c=(1,1,1)):
         self.vertices.append(v)
+        self.velocities.append(p)
+        self.colors.append(c)
         return len(self.vertices) - 1
     
     def add_triangle(self,index1, index2, index3):
@@ -19,24 +23,44 @@ class Mesh:
 
     def as_np_arrays(self):
         vs = np.empty((len(self.vertices), 2))
+        ps = np.empty((len(self.vertices), 2))
+        cs = np.empty((len(self.vertices), 3))
         inds = np.empty(len(self.indices))
 
         for i,(x,y) in enumerate(self.vertices):
             vs[i, 0] = x
             vs[i, 1] = y
+            p_x, p_y = self.velocities[i]
+            ps[i, 0] = p_x
+            ps[i, 1] = p_y
+            r,g,b = self.colors[i]
+            cs[i, 0] = r
+            cs[i, 1] = g
+            cs[i, 2] = b
+
+
+
 
         for i, ind in enumerate(self.indices):
             inds[i] = ind
 
-        return vs, inds
+        return vs, ps, inds, cs
 
-    def plane(offset, w, h, size):
+    def plane(center, w, h, size, rot_vel=0):
         mesh = Mesh()
+
+        center_x, center_y = center
+        
+        s_x = center_x - size * w / 2
+        s_y = center_y - size * h / 2
 
         for i in range(w):
             for j in range(h):
-                mesh.add_vertex((offset + i * size, offset + j * size));
-                
+                v_x = s_x + i * size
+                v_y = s_y + j * size
+
+                mesh.add_vertex((v_x, v_y), (rot_vel*(v_y-center_y), rot_vel*(center_x-v_x)), (i/w, j/h, 0))
+
                 if i < w - 1 and j < h - 1:
                     mesh.add_triangle((i + 1) * h + j, i * h + j, i * h + j + 1)
                     mesh.add_triangle((i + 1) * h + j, i * h + j + 1, (i+1)*h + j + 1)
@@ -53,17 +77,23 @@ class SoftbodySim:
         self.displacement = ti.Vector.field(2, ti.f32, shape=self.n, needs_grad=True)
         self.velocities = ti.Vector.field(2, ti.f32, shape=self.n)
         self.positions = ti.Vector.field(2, ti.f32, shape=self.n)
+        self.colors = ti.Vector.field(3, ti.f32, shape=self.n)
 
         self.A = ti.Matrix.field(2,2, ti.f32, shape=self.tris)
         self.V = ti.field(ti.f32, shape=self.tris)
+        
         self.E = ti.field(ti.f32, shape=(), needs_grad=True)
+        self.E_m = ti.field(ti.f32, shape=(), needs_grad=True)
+        self.E_c = ti.field(ti.f32, shape=(), needs_grad=True)
 
         self.indices = ti.field(ti.i32, shape=len(mesh.indices))
         
-        vs, inds = mesh.as_np_arrays()
+        vs, ps, inds, cs = mesh.as_np_arrays()
 
         self.vertices.from_numpy(vs)
         self.indices.from_numpy(inds)
+        self.velocities.from_numpy(ps)
+        self.colors.from_numpy(cs)
 
         self.init()
    
@@ -79,13 +109,10 @@ class SoftbodySim:
             A = ti.Matrix([[e1[0], e2[0]], [e1[1], e2[1]]])
             self.A[tri] = A.inverse()
             self.V[tri] = (e1[0] * e2[1] - e1[1] * e2[0]) / 2
-            print(A)
-            print(self.A[tri])
-            print(self.V[tri])
 
        
     @ti.kernel
-    def calculate_energy(self): 
+    def calculate_mesh_energy(self): 
         for tri in range(self.tris):
             x1 = self.vertices[self.indices[3 * tri]] + self.displacement[self.indices[3 * tri]]
             x2 = self.vertices[self.indices[3 * tri + 1]] + self.displacement[self.indices[3 * tri + 1]]
@@ -97,39 +124,51 @@ class SoftbodySim:
             B = ti.Matrix([[e1[0], e2[0]], [e1[1], e2[1]]])
             F = B @ self.A[tri] 
             
-            E = 0.5 * (F.transpose() @ F - ti.Matrix.identity(ti.f32, 2)) #Green Strain not energy
-            self.E[None] += (0.05*(E.trace())**2 + 0.1*(E@E).trace()) * self.V[tri] 
+            #E = 0.5 * (F.transpose() @ F - ti.Matrix.identity(ti.f32, 2)) #Green Strain not energy
+            #self.E[None] += 0.1*((F.determinant() - 1)**2 + (E@E).trace()) * self.V[tri] 
 
+            mu = 0.05
+            lam = 0.01
+
+            self.E_m[None] += 0.5 * mu * ((F.transpose() @ F).trace() - 3) - mu * ti.log(F.determinant()) + lam * ti.log(F.determinant())**2 + 0.01 * (x1[1]+x2[1]+x3[1])
+
+
+    @ti.kernel
+    def calculate_energy(self):
+        self.E[None] = self.E_m[None]
+    
     @ti.kernel
     def step(self):
         for i in self.vertices: 
-            self.velocities[i] -= (self.displacement.grad[i] + 10 * self.velocities[i]) * self.dt
+            self.velocities[i] -= (self.displacement.grad[i] + 0.2 * self.velocities[i]) * self.dt
             self.displacement[i] += self.velocities[i] * self.dt
 
-            if self.positions[i][1] < 0.1:
-                self.velocities[i] += ti.Matrix([0.0,0.1]) * self.dt
-            else:
-                self.velocities[i] -= ti.Matrix([0.0,0.01]) * self.dt
+            if self.positions[i][1] < 0.0:
+                self.velocities[i] += ti.Matrix([0.0,1.0]) * self.dt
 
             self.positions[i] = self.vertices[i] + self.displacement[i] 
 
 if __name__ == "__main__":
-    ti.init() 
-    mesh = Mesh.plane(0.3,10,10,0.04)
+    ti.init(arch=ti.cuda) 
+    mesh = Mesh.plane((0.5,2.0),3,100,0.02)
 
-    sim = SoftbodySim(mesh, dt=0.01)
+    sim = SoftbodySim(mesh, dt = 0.005)
 
-    window = ti.ui.Window("float",res = (500,500))
+    window = ti.ui.Window("float",res = (1000 , 1000))
     canvas = window.get_canvas()
         
     i = 0
     while window.running:
         sim.E[None] = 0
+        sim.E_m[None] = 0
+        sim.E_c[None] = 0
+
         with ti.Tape(sim.E):
+            sim.calculate_mesh_energy()
             sim.calculate_energy()
         sim.step()
 
-        canvas.triangles(sim.positions, color=(1.0,1.0,1.0), indices=sim.indices)
+        canvas.triangles(sim.positions, indices=sim.indices, per_vertex_color=sim.colors)
         window.show()
 
         i += 1
