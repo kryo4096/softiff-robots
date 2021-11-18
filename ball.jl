ab7d73bf075a7e00e40707b0208de45e8e74de0a
Jonas Luther <kryo4096@gmail.com>
2021-11-18 23:16:22 +0100

    changed thingos

--- ball.jl
+++ ball.jl
@@ -1,52 +1,33 @@
 using GLMakie
 using Zygote
 
+function integrate(force, x0, v0, dt, n)
+    x = x0
+    v = v0
+    t = 0.0
 
-function step(x, v, g, Δt)
-    x1 = x + v * Δt
-    v1 = v + [0, -g] * Δt
-
-    if x[2] < 0
-        v1 +=  [10 * v1[1] * x[2], 100] * Δt
-    end   
-
-    return x1, v1
-end
-
-function ball_throw(α, v, g, Δt, t_1) 
-    pos = zeros(2)
-    vel = v * [cos(α), sin(α)]
-
-    for i ∈ 1:length(0:Δt:t_1)
-        pos, vel = step(pos, vel, g, Δt)
-    end
-
-    return pos[1]
-end
-
-function ball_throw_traj(α, v, g, Δt, t_1) 
-    pos = zeros(2)
-    vel = v * [cos(α), sin(α)]
-
-    ts = 0:Δt:t_1
-
-    n = length(ts)
-
-    traj = zeros(n, 2)
+    trajectory = zeros(n, 2)
 
     for i ∈ 1:n
-        pos, vel = step(pos, vel, g, Δt)
+        x += v * dt
+        v += force(x, v, t) * dt
 
-        traj[i,:] = pos
+        Zygote.ignore() do 
+            trajectory[i,:] = x
+        end
+
+        t += dt
     end
 
-    return traj
+    return trajectory, x
+
 end
+
 
 function main() 
 
-    Δt = 0.01
-    t_1 = 2.0
+    dt = 0.001
+    n = 2000
     v = 1
     g = 1.0
 
@@ -55,20 +36,35 @@ function main()
     f = Figure()
     Axis(f[1,1])
 
-    traj = Node(zeros(length(0:Δt:t_1), 2))
+    traj = Node(zeros(n, 2))
 
-    display(lines(traj, axis=(;limits=(0.0, v * t_1, -0.2, 0.5 * v^2 / g))))
+    display(lines(traj, axis=(;limits=(0.0, 1.0, 0.0, 1.0))))
 
-    dist(x) = ball_throw(x, v, g, Δt, t_1)
+    function gravity(x, v, t) 
+        f = [0,0]
 
+        if x[2] < 0 
+            f += v * x[2] * 100000
+        else 
+            f += [0, -g]
+        end
+
+        return f
+    end
+
+    ball_throw(x) = integrate(gravity, [0.0,0.0], v * [cos(x), sin(x)], dt, n)
+
+    da_old = 0
     for k ∈ 1:100000
-        δα = dist'(α)
-        α += δα * 0.0001
+        traj[], p = ball_throw(α)
+        da = gradient(x -> ball_throw(x)[2][1], α)[1]
+
+        α += 0.005 * da / max(abs(da_old - da), 1)
+
+        da_old = da
         
-        traj[] = ball_throw_traj(α, v, g, Δt, t_1)
-    
-        sleep(0.001)
+        println("α = $(α / 2π * 360)°")
     end
 end
 
-main()
+main()    
