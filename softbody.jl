829aa77bf4e30807b26c2a4c5813c84566fb7fb1
Jonas Luther <kryo4096@gmail.com>
2021-11-26 17:53:59 +0100

    changed to linear vertex layout

--- softbody.jl
+++ softbody.jl
@@ -8,12 +8,12 @@ mutable struct Simulation{S <: AbstractFloat,I <: Integer}
     g::S
     dt::S
 
-    X::Matrix{S}
-    D::Matrix{S}
-    V::Matrix{S}
+    X::Vector{S}
+    D::Vector{S}
+    V::Vector{S}
     M::Vector{S}
 
-    ind::Matrix{I}
+    ind::Vector{SVector{3, I}}
     A_inv::Vector{SMatrix{2,2,S,4}}
     vol::Vector{S}
     lambda::Vector{S}
@@ -21,19 +21,23 @@ mutable struct Simulation{S <: AbstractFloat,I <: Integer}
 end
 
 
-edge_mat(x) = SMatrix{2,2}([x[:, 2] - x[:, 1] x[:, 3] - x[:, 1]])
+index_arr(ind) = SA[2 * ind[1] - 1, 2 * ind[1], 2 * ind[2] - 1, 2 * ind[2], 2 * ind[3] - 1, 2 * ind[3]]
+edge_mat(x) = SA[x[3]-x[1] x[5]-x[1]; x[4]-x[2] x[6]-x[2]]
 
 function compute_gradient_parallel!(grad, sim::Simulation, D_1, a=0.01)
-    N_p = size(sim.X)[2]
-    N_t = size(sim.ind)[2]
+    N_p = size(sim.X)[1]÷2
+    N_t = size(sim.ind)[1]
 
     lk = ReentrantLock()
 
     # per-triangle gradient
 
     Threads.@threads for ti = 1:N_t
-        x_0 = sim.X[:, sim.ind[:, ti]]
-        d_1 = D_1[:, sim.ind[:, ti]]
+
+        inds = index_arr(sim.ind[ti])
+
+        x_0 = sim.X[inds]
+        d_1 = D_1[inds]
 
         function E(d)
             F = edge_mat(x_0 + d) * sim.A_inv[ti]
@@ -50,17 +54,20 @@ function compute_gradient_parallel!(grad, sim::Simulation, D_1, a=0.01)
         contrib = sim.dt^2 * sim.vol[ti] * ForwardDiff.gradient(E, d_1)
 
         lock(lk) do
-            grad[:, sim.ind[:, ti]] += contrib
+            grad[inds] += contrib
         end
     end
 
     # per-vertex gradient
 
     Threads.@threads for vi = 1:N_p
-        x_0 = sim.X[:, vi]
-        v_0 = sim.V[:, vi]
-        d_0 = sim.D[:, vi]
-        d_1 = D_1[:, vi]
+
+        inds = SA[2*vi - 1, 2 * vi]
+
+        x_0 = sim.X[inds]
+        v_0 = sim.V[inds]
+        d_0 = sim.D[inds]
+        d_1 = D_1[inds]
         m = sim.M[vi]
 
         function E(d)
@@ -87,84 +94,10 @@ function compute_gradient_parallel!(grad, sim::Simulation, D_1, a=0.01)
         contrib = ForwardDiff.gradient(E, d_1)
 
         lock(lk) do
-            grad[:, vi] += contrib
+            grad[inds] += contrib
         end
 
     end
-end
-    
-function compute_hxv!(V, sim::Simulation, D_1, a=0.01)
-    N_p = size(sim.X)[2]
-    N_t = size(sim.ind)[2]
-
-    lk = ReentrantLock()
-
-    prod = zeros(size(sim.X))
-
-    # per-triangle gradient
-
-    Threads.@threads for ti = 1:N_t
-        x_0 = sim.X[:, sim.ind[:, ti]]
-        d_1 = D_1[:, sim.ind[:, ti]]
-        v = V[:, sim.ind[:, ti]]
-
-        function E(d)
-            F = edge_mat(x_0 + d) * sim.A_inv[ti]
-
-            dF = max(det(F), 0.1)
-
-            lJ = dF > a ? log(dF) : log(a) + 1 / a * (dF - a) - 1 / a^2 * (dF - a)^2
-
-            E = sim.mu[ti] * (0.5 * (tr(F' * F) - 3.0) - lJ) + 0.5 * sim.lambda[ti] * lJ^2
-
-            return sim.dt^2 * sim.vol[ti] * E
-        end
-
-        contrib = gradient(d -> sum(ForwardDiff.gradient(E, d) .* v, d_1))
-
-        lock(lk) do
-            prod[:, sim.ind[:, ti]] += contrib
-        end
-    end
-
-    # per-vertex gradient
-
-    Threads.@threads for vi = 1:N_p
-        x_0 = sim.X[:, vi]
-        v_0 = sim.V[:, vi]
-        d_0 = sim.D[:, vi]
-        d_1 = D_1[:, vi]
-        m = sim.M[vi]
-        v = V[:, sim.ind[:, ti]]
-
-        function E(d)
-
-            I = 0.5 * sum((d - (d_0 + v_0 * sim.dt)).^2) * m
-
-            p = x_0 + d
-
-            E = 0.0
-
-            v .= (d .- d_0) / sim.dt
-
-            if p[2] > 0.0
-                E += sim.g * p[2] * m
-            else
-                E += 100000 * (0.0 - p[2])^2 - v[1]^2 * p[2] * 1000
-            end
-
-            return I + sim.dt^2 * E
-        end
-
-         contrib = gradient(d -> sum(ForwardDiff.gradient(E, d) .* v, d_1))
-
-        lock(lk) do
-            prod[:, vi] += contrib
-        end
-
-    end
-
-    return prod
 end
 
 function init_simulation(; g=9.8, dt=0.01, center=[0.5, 0.5], radius=0.5, mu=100, lam=50, n=20)::Simulation
@@ -172,25 +105,26 @@ function init_simulation(; g=9.8, dt=0.01, center=[0.5, 0.5], radius=0.5, mu=100
     n_vertices = n + 1
     n_triangles = n
 
-    X = zeros(2, n_vertices)
-    D = zeros(2, n_vertices)
-    V = zeros(2, n_vertices)
+    X = zeros(2 * n_vertices)
+    D = zeros(2 * n_vertices)
+    V = zeros(2 * n_vertices)
     M = ones(n_vertices)
+    
+    ind = fill(SA[0,0,0], n_triangles)
 
-    ind = zeros(Int, 3, n_triangles)
-
-    X[:, 1] = center
+    X[[1,2]] = center
 
     for i = 1:n
+        vi = [2*(i+1) - 1, 2*(i+1)]
         angle = i / n * 2π
-        X[:, i + 1] = radius * [cos(angle), sin(angle)] + center
-        V[:, i + 1] = 5 * [-sin(angle), cos(angle)] + [0,1]
-        ind[:, i] = [1, 1 + i, 1 + mod1(i + 1, n)]
+        X[vi] .= radius * [cos(angle), sin(angle)] + center
+        V[vi] .= 5 * [-sin(angle), cos(angle)] + [0,1]
+        ind[i] = SA[1, 1 + i, 1 + mod1(i + 1, n)]
     end
 
     lambda = ones(n_triangles) * lam
     mu = ones(n_triangles) * mu
-    A = map(ti -> edge_mat(X[:, ind[:, ti]]), 1:n_triangles)
+    A = map(ti -> edge_mat(X[index_arr(ind[ti])]), 1:n_triangles)
     A_inv = inv.(A)
 
     vol = abs.(0.5 * det.(A))
@@ -243,13 +177,13 @@ end
 function main()
     sim = init_simulation(; center=[0.5, 0.5], dt=0.001, lam=20000.0, mu=10000.0, g=10.0, n=10, radius=0.3)
 
-    render_pos = Node(sim.X + sim.D)
+    render_pos = Node(reshape(sim.X + sim.D, 2, size(sim.X)[1]÷2))
 
     f = Figure(resolution=(500, 500))
     mu_slider = Slider(f[2, 1], range=500:10:10000, startvalue=3000)
     ax = Axis(f[1, 1], aspect=1, limits=(-0.1, 1.1, -0.1, 1.1))
 
-    mesh!(f[1, 1], render_pos, sim.ind[:])
+    mesh!(f[1, 1], render_pos, Vector(vcat(sim.ind...)))
     display(f)
 
     for i = 1:1000000
@@ -261,7 +195,7 @@ function main()
         if i % 1 == 0
             sim.lambda .= mu_slider.value[]
             sim.mu .= mu_slider.value[]
-            render_pos[] = sim.X + sim.D
+            render_pos[] = reshape(sim.X + sim.D, 2, size(sim.X)[1]÷2)
             sleep(0.001)
         end
     end
