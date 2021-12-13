7e9cf366593f1bcb6c795fa84fcfa68cd93426fe
Jonas Luther <kryo4096@users.noreply.github.com>
2021-12-13 22:18:09 +0100

    dontknow

--- src/simulation.jl
+++ src/simulation.jl
@@ -1,8 +1,7 @@
-
-
 module Simulation
     using JSON
     using GLMakie
+    using LinearAlgebra
 
     include("softbody.jl")
 
@@ -17,7 +16,37 @@ module Simulation
         vertices = [Float64(x) for x in robot["vertices"]]
         actuators = [(Int64(a[1]), Int64(a[2])) for a in robot["actuators"]]
 
-        vertices, indices,  actuators
+        vertices, indices, actuators
+    end
+
+    function plot_actuators(fig_location, a, vertices)
+
+        actuator_indices = Vector{Int64}()
+        for i = 1:length(a)
+            append!(actuator_indices, [0, -1, -2, 0, -2, -3] .+ 4 * i)
+        end
+
+        mesh!(
+            fig_location, 
+            lift(vertices) do v
+                verts = Matrix{Float64}(undef, 2, 0)
+
+                for (i, j) in a
+                    vi = v[:, i]
+                    vj = v[:, j]
+
+                    dir = normalize(vj - vi)
+
+                    delta = [-dir[2], dir[1]] * 0.01
+
+                    verts = [verts vi - delta vi + delta vj - delta vj + delta]
+                end
+
+                verts
+            end,
+            actuator_indices,
+        )
+
     end
 
     function run(filename)
@@ -38,13 +67,18 @@ module Simulation
             end
         end
 
-        mesh(fig[1,1], mv, i)
+        ax = Axis(fig[1,1], aspect=1.0, limits=(-0.55, 0.55, -0.1, 1.0))
+        deactivate_interaction!(ax, :rectanglezoom)
+
+        mesh!(fig[1,1], mv, i)
+
+        plot_actuators(fig[1,1], a, mv)
 
         display(fig)
 
         i = 0
         while running[]
-            Softbody.step!(sim, ones(size(sim.a)))
+            Softbody.step!(sim, 1 .+ 0.4 .* sin.(i/100 .+ (1:length(a))))
 
             if i%10==0
                 mv[] = Softbody.render_verts(sim)
--- src/softbody.jl
+++ src/softbody.jl
@@ -5,7 +5,7 @@ module Softbody
     using StaticArrays
     using IterativeSolvers
     using SparseArrays
-
+    using Printf
 
     mutable struct Simulation{S <: AbstractFloat,I <: Integer}
         g::S
@@ -31,8 +31,7 @@ module Softbody
         spring_stiffness::S
     end 
 
-    function create_simulation(vertices::Vector, indices::Vector, actuators::Vector, ;g=9.8, floor_force=1e4, floor_height=0.0, floor_friction=1e2, dt=1e-3, lambda=1e5, mu=1e5, spring_stiffness=1e2, m = 1)
-        
+    function create_simulation(vertices::Vector, indices::Vector, actuators::Vector, ;g=20.0, floor_force=5e3, floor_height=0.0, floor_friction=5e3, dt=0.005, lambda=5e3, mu=5e3, spring_stiffness=1e5, m = 1)
         D = zeros(size(vertices))
         V = zeros(size(vertices))
         M = ones(size(vertices)[1]÷2) * m
@@ -75,9 +74,9 @@ module Softbody
 
         current_length = sqrt((d_p[3]-d_p[1])^2 + (d_p[4]-d_p[2])^2)
 
-        E = 0.5* spring_stiffness * (current_length/(initial_length * actuation) - 1)^2
+        E = 0.5 * spring_stiffness * (current_length/(initial_length * actuation) - 1)^2
 
-        return dt^2 * E
+        return E
     end
 
     function vertex_energy(d, d_0, v_0, x_0, dt, m, floor_height, floor_force, floor_friction, g)  
@@ -92,7 +91,7 @@ module Softbody
             E += floor_force * (floor_height - p[2])^2 + v[1]^2 * (floor_height - p[2]) * floor_friction
         end
 
-        return I + dt^2 * E
+        return I / dt^2 + E
     end
 
     function compute_gradient!(grad, sim::Simulation, D_1, a=0.01)
@@ -100,11 +99,9 @@ module Softbody
         N_t = size(sim.ind)[1]
         N_a = size(sim.a)[1]
 
-        lk = ReentrantLock()
-
         # per-triangle gradient
 
-        Threads.@threads for ti = 1:N_t
+        for ti = 1:N_t
 
             inds = index_arr(sim.ind[ti])
 
@@ -113,15 +110,11 @@ module Softbody
 
             E(d) = triangle_energy(d, sim.A_inv[ti], x_0, a, sim.lambda[ti], sim.mu[ti])
 
-            contrib = sim.dt^2 * sim.vol[ti] * ForwardDiff.gradient(E, d_1)
-
-            lock(lk) do
-                grad[inds] += contrib
-            end
+            grad[inds] += sim.vol[ti] * ForwardDiff.gradient(E, d_1)
         end
         
         # per-actuation gradient
-        Threads.@threads for ai = 1:N_a
+        for ai = 1:N_a
             row = sim.actuators_i[ai]
             col = sim.actuators_j[ai]
             act = sim.a[ai]
@@ -132,16 +125,12 @@ module Softbody
             d_1 = D_1[inds]
 
             E(d) = actuation_energy(d, sim.spring_stiffness, x_0, act, sim.dt)
-
-            contrib = ForwardDiff.gradient(E, d_1)
-
-            lock(lk) do 
-                grad[inds] += contrib 
-            end
+            
+            grad[inds] += ForwardDiff.gradient(E, d_1)
         end
 
         # per-vertex gradient
-        Threads.@threads for vi = 1:N_p
+        for vi = 1:N_p
 
             inds = SA[2 * vi - 1, 2 * vi]
 
@@ -151,44 +140,8 @@ module Softbody
             d_1 = D_1[inds]
             
             E(d) = vertex_energy(d, d_0, v_0, x_0, sim.dt, sim.M[vi], sim.floor_height, sim.floor_force, sim.floor_friction, sim.g)
-
-            contrib = ForwardDiff.gradient(E, d_1)
-
-            lock(lk) do
-                    grad[inds] += contrib
-            end
+            grad[inds] += ForwardDiff.gradient(E, d_1)
         end
-    end
-   
-    function init_hessian!(hess, sim::Simulation, init_val=1)
-
-        N_p = size(sim.X)[1] ÷ 2
-        N_t = size(sim.ind)[1]
-        N_a = size(sim.a)[1]
-
-        for ti = 1:N_t
-            inds = index_arr(sim.ind[ti])
-
-            hess[inds, inds] .= init_val
-        end
-
-        for ai = 1:N_a
-            row = sim.actuators_i[ai]
-            col = sim.actuators_j[ai]
-
-            inds = SA[2 * col - 1, 2 * col, 2*row - 1, 2*row]
-
-            hess[inds, inds] .= init_val
-        end
-
-        for vi = 1:N_p
-
-            inds = SA[2 * vi - 1, 2 * vi]
-
-            hess[inds, inds] .= init_val
-        end
-
-        return hess
     end
 
     function compute_hessian!(hess, sim::Simulation, D_1, a=0.01)
@@ -196,13 +149,8 @@ module Softbody
         N_t = size(sim.ind)[1]
         N_a = size(sim.a)[1]
 
-        lk = ReentrantLock()
-
-        init_hessian!(hess, sim, 0)
-
-        # per-triangle gradient
-
-        Threads.@threads for ti = 1:N_t
+        #per-triangle gradient
+        for ti = 1:N_t
 
             inds = Vector(index_arr(sim.ind[ti]))
 
@@ -211,15 +159,11 @@ module Softbody
 
             E(d) = triangle_energy(d, sim.A_inv[ti], x_0, a, sim.lambda[ti], sim.mu[ti])
 
-            contrib = sim.dt^2 * sim.vol[ti] * ForwardDiff.hessian(E, d_1)
-
-            lock(lk) do
-                hess[inds, inds] .+= contrib
-            end
+            hess[inds, inds] .+= sim.vol[ti] * ForwardDiff.hessian(E, d_1)
         end
 
         #Per-actuation gradient
-        Threads.@threads for ai = 1:N_a
+        for ai = 1:N_a
             row = sim.actuators_i[ai]
             col = sim.actuators_j[ai]
             act = sim.a[ai]
@@ -231,16 +175,12 @@ module Softbody
 
             E(d) = actuation_energy(d, sim.spring_stiffness, x_0, act, sim.dt)
 
-            contrib = ForwardDiff.hessian(E, d_1)
-
-            lock(lk) do 
-                hess[inds, inds] .+= contrib
-            end
+            hess[inds, inds] .+= ForwardDiff.hessian(E, d_1)
         end
 
         # per-vertex gradient
 
-        Threads.@threads for vi = 1:N_p
+        for vi = 1:N_p
 
             inds = [2 * vi - 1, 2 * vi]
 
@@ -251,63 +191,72 @@ module Softbody
 
             E(d) = vertex_energy(d, d_0, v_0, x_0, sim.dt, sim.M[vi], sim.floor_height, sim.floor_force, sim.floor_friction, sim.g)
 
-            contrib = ForwardDiff.hessian(E, d_1)
-
-            lock(lk) do
-                hess[inds, inds] .+= contrib
-            end
-
+            hess[inds, inds] .+= ForwardDiff.hessian(E, d_1)
         end
     end
-        
-    struct Pass
-        alpha
-        iter
-        guess
-        tol
+
+    function dfda(D, A)
+        grad = zeros(length(sim.X), length(sim.a))
+
+        # per-actuation gradient
+        for ai = 1:N_a
+            row = sim.actuators_i[ai]
+            col = sim.actuators_j[ai]
+            act = A[ai]
+
+            inds = SA[2 * col - 1, 2 * col, 2*row - 1, 2*row]
+
+            x_0 = sim.X[inds]
+            d_1 = D_1[inds]
+
+            function E(d)
+                E(a) = actuation_energy(d, sim.spring_stiffness, x_0, a, sim.dt)
+                return ForwardDiff.gradient(E1, act)
+            end
+            
+            grad[inds, ai] += ForwardDiff.gradient(E, d_1)
+        end
     end
 
-    function newton!(hess, hessian!, gradient!, x_guess, tol)
+    function newton(sim, hessian!, gradient!, x_guess, tol, iterations)
         x = x_guess
         grad = zeros(size(x))
+        hess = zeros(length(sim.X), length(sim.X))
 
-        for i in 1:10
+        for _ in 1:iterations
             grad .= 0
+            hess .= 0
             gradient!(grad, x)
             hessian!(hess, x)
 
             x .= x .- cg(hess, grad)
 
-            if norm(grad) < tol
+            if norm(grad) * sim.dt^2 < tol
                 break
             end
         end
-
-        if norm(grad) > tol
-            println("WARNING: No convergence!")
+        
+        if norm(grad) * sim.dt^2 > tol
+            @printf "|∇E| = %.1e\n" norm(grad) * sim.dt^2
         end
 
         return x
     end
 
-    function step!(sim::Simulation, a::Vector; tol=1e-4)
+    function step!(sim::Simulation, a::Vector; tol=1e-6)
         sim.a = a
-        
-        hessian = spzeros(length(sim.X), length(sim.X))
-        init_hessian!(hessian, sim, 1.0)
-        grad = zeros(length(sim.X))
-
-        D = newton!(
-            hessian,
+   
+        D = newton(
+            sim,
             (h, d) -> compute_hessian!(h, sim, d),
             (g, d) -> compute_gradient!(g, sim, d),
-            sim.D + sim.V * sim.dt,
-            tol
+            sim.D,
+            tol,
+            2
         )
 
         sim.V .= (D .- sim.D) ./ sim.dt
         sim.D .= D
     end
-
 end
 
